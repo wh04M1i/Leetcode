f5de397bc47cf8939763bfa87900a92de1e6d0f3
Goverdhan Kumar <58867508+wh04M1i@users.noreply.github.com>
2023-09-02 11:19:39 +0530

    Time: 11 ms (96.92%), Space: 19.2 MB (34.86%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -2,15 +2,18 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         int n = nums.size();
-        int left = 0;  // Pointer to the leftmost zero
-        int right = 0; // Pointer to the current element being processed
-        
-        while (right < n) {
-            if (nums[right] != 0) {
-                swap(nums[left], nums[right]);
-                left++;
+        int nonZeroIndex = 0;
+
+        // Move all non-zero elements to the left
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != 0) {
+                nums[nonZeroIndex++] = nums[i];
             }
-            right++;
+        }
+
+        // Fill the remaining positions with zeros
+        for (int i = nonZeroIndex; i < n; i++) {
+            nums[i] = 0;
         }
     }
 };
