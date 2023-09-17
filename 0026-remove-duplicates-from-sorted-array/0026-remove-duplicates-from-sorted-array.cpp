858af79d95f05f4af36e0537aee1607e4319a40f
Goverdhan Kumar <58867508+wh04M1i@users.noreply.github.com>
2023-09-17 21:15:52 +0530

    Time: 7 ms (80.10%), Space: 18.7 MB (14.41%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int size = nums.size();
-        int i=0;
-        while(i<size-1){
-            if(nums[i]==nums[i+1])
-            {
-             for(int j=i;j<size-1;j++)   
-              nums[j]=nums[j+1];
-                size--;
-            }
-        else
-            i++;
+  if (nums.empty()) {
+            return 0;
         }
-        return size;
-    }
+        
+        int uniqueIndex = 0; // Index to keep track of the last unique element
+        
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] != nums[uniqueIndex]) {
+                uniqueIndex++;
+                nums[uniqueIndex] = nums[i];
+            }
+        }
+        
+        // uniqueIndex + 1 gives the count of unique elements
+        return uniqueIndex + 1;}
+        
+       
+        
 };
