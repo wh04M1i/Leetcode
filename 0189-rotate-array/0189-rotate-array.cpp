89f989b1c0ad01de601166c24d91562aefa8db68
Goverdhan Kumar <58867508+wh04M1i@users.noreply.github.com>
2023-09-20 11:29:26 +0530

    Time: 19 ms (85.27%), Space: 25.5 MB (33.75%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -18,26 +18,26 @@ public:
             // Handle the case where k is larger than the array size
         k = k % n;
         
-//         // Create a temporary vector to hold the rotated elements
-//         std::vector<int> temp(nums.begin() + n - k, nums.end());
+        // Create a temporary vector to hold the rotated elements
+        std::vector<int> temp(nums.begin() + n - k, nums.end());
         
-//         // Move the first part of the array to the right
-//         for (int i = n - k - 1; i >= 0; i--) {
-//             nums[i + k] = nums[i];
-//         }
+        // Move the first part of the array to the right
+        for (int i = n - k - 1; i >= 0; i--) {
+            nums[i + k] = nums[i];
+        }
         
-//         // Copy the rotated elements from the temporary vector back to the original vector
-//         for (int i = 0; i < k; i++) {
-//             nums[i] = temp[i];
-//         }
+        // Copy the rotated elements from the temporary vector back to the original vector
+        for (int i = 0; i < k; i++) {
+            nums[i] = temp[i];
+        }
         
-         // Reverse the entire array
-        reverse(nums.begin(), nums.end());
+//          // Reverse the entire array
+//         reverse(nums.begin(), nums.end());
         
-        // Reverse the first k elements
-        reverse(nums.begin(), nums.begin() + k);
+//         // Reverse the first k elements
+//         reverse(nums.begin(), nums.begin() + k);
         
-        // Reverse the remaining elements
-        reverse(nums.begin() + k, nums.end());
+//         // Reverse the remaining elements
+//         reverse(nums.begin() + k, nums.end());
     }
 };
