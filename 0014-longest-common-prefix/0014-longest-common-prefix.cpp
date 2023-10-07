3ad31847be789c4bec2a6327c2b8bc1daa8d70c7
Goverdhan Kumar <58867508+wh04M1i@users.noreply.github.com>
2023-10-07 10:36:27 +0530

    Time: 6 ms (34.35%), Space: 12.6 MB (7.81%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,47 +1,27 @@
-#include <vector>
-#include <string>
-
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        // Check for empty input
-        if (strs.empty()) {
-            return "";
-        }
-        
-        // Find the shortest string in the vector
-        string shortest = strs[0];
-        for (string s : strs) {
-            if (s.length() < shortest.length()) {
-                shortest = s;
-            }
-        }
-        
-        // Use binary search to find the longest common prefix
-        int low = 0;
-        int high = shortest.length() - 1;
-        
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            string prefix = shortest.substr(0, mid + 1);
-            
-            bool isCommonPrefix = true;
-            for (string s : strs) {
-                if (s.substr(0, mid + 1) != prefix) {
-                    isCommonPrefix = false;
-                    break;
+        string longestprefix = "";
+        int s = strs.size();
+
+        if (s == 0)
+            return longestprefix;
+
+        if (s == 1)
+            return strs[0];
+
+        int n = strs[0].size();
+        for (int i = 1; i <= n; i++) {
+            string temp = strs[0].substr(0, i);
+
+            for (int j = 1; j < s; j++) {
+                if (i > strs[j].size() || temp != strs[j].substr(0, i)) {
+                    return longestprefix;
                 }
             }
-            
-            if (isCommonPrefix) {
-                // If it's a common prefix, search the right half
-                low = mid + 1;
-            } else {
-                // If it's not a common prefix, search the left half
-                high = mid - 1;
-            }
+            longestprefix = temp;
         }
-        
-        return shortest.substr(0, low);
+
+        return longestprefix;
     }
 };
